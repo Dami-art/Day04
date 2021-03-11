d1a1f1c10b6a54b9ed93966713f815f3dceda8b5
Dami-art <damjana.zoric@gmail.com>
2021-03-11 22:15:40 +0100

    commi

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main (){
+//Only change code bellow this line
+var myFirstName = "Damjana";
+var myLastName = "Zoric";
+var myHomeTown = "Sarajevo";
+//Only change code above this line
+return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+};
+}
+
+
+
+console.log (main());
+module.exports = main;
