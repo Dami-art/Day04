d8f56c2b64b91435ef158cac2f203ec3e8d23ebc
Dami-art <damjana.zoric@gmail.com>
2021-03-11 20:23:51 +0100

    nove izmjene

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,7 +1,7 @@
 function main(numOne, numTwo){
 
  var quotient;
- if numTwo === 0){
+ if (numTwo === 0){
 quotient = "Divide by 0! Cannot divide by zero!"
 } else {
     quotient = numOne/numTwo;
@@ -12,5 +12,8 @@ return quotient;
 
 }
 console.log(main(numOne, numTwo));
-
+console.log(main(15, 3))
+console.log(main(25, 5))
+console.log(main(420, 10))
+console.log(main(55, 0))
 module.exports = main;
