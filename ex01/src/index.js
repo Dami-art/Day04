4a327cd8924d9652de9c89670f5154f46ae9f5e9
Dami-art <damjana.zoric@gmail.com>
2021-03-11 21:06:15 +0100

    New commit exp

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,19 +1,21 @@
-function main(numOne, numTwo){
+function main (numOne, numTwo){
 
  var quotient;
  if (numTwo === 0){
 quotient = "Divide by 0! Cannot divide by zero!"
 } else {
-    quotient = numOne/numTwo;
+    quotient = numOne / numTwo;
 }
 
 
 return quotient;
 
 }
-console.log(main(numOne, numTwo));
-console.log(main(15, 3))
-console.log(main(25, 5))
-console.log(main(420, 10))
-console.log(main(55, 0))
+
+var numOne
+console.log(main (15, 3));
+console.log(main(25, 5));
+console.log(main(420, 10));
+console.log(main(55, 0));
+
 module.exports = main;
---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+function main (){
+//only change code below this line
+var myDecimal = 2.5
+var myDecimalE = 2.54E-3 
+//Only change code above this line
+return {
+    myDecimal,
+    myDecimalE
+
+};
+
+}
+
+
+console.log(main ());
+module.exports = main;
